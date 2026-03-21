5d31409642c7c7298acacd4ae5341af9efffe8e6
Hernan Fernandez <hernanjx@gmail.com>
2026-03-21 19:49:42 -0300

    Review: remove flaky probabilistic uniqueness test

--- shortener/shortener_test.go
+++ shortener/shortener_test.go
@@ -24,23 +24,6 @@ func TestGenerateCharset(t *testing.T) {
 	}
 }
 
-func TestGenerateUniqueness(t *testing.T) {
-	seen := make(map[string]bool)
-	collisions := 0
-	n := 1000
-	for i := 0; i < n; i++ {
-		code := Generate()
-		if seen[code] {
-			collisions++
-		}
-		seen[code] = true
-	}
-	// With 62^6 possibilities and 1000 samples, collisions should be extremely rare
-	if collisions > 5 {
-		t.Errorf("too many collisions: %d out of %d", collisions, n)
-	}
-}
-
 func contains(s string, r rune) bool {
 	for _, c := range s {
 		if c == r {
